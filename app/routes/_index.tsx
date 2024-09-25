729c31e199ee84cd521ae1ce416afd6440220592
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-09-25 18:20:01 +0300

    add defer example to show `Suspense` error with `AppShell`

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,6 @@
-import {defer, LoaderFunction, MetaFunction} from "@remix-run/node";
+import {defer, LoaderFunctionArgs, MetaFunction} from "@remix-run/node";
 import {AppShell, Container} from "@mantine/core";
-import {getMockData} from "~/utils/mock.server";
-import {Await, useLoaderData, useNavigation} from "@remix-run/react";
-import {ChartData, lastWeekData, thisWeekData} from "~/utils/mockData";
-import {Charts} from "~/components/Charts/Charts";
+import {Await, useLoaderData} from "@remix-run/react";
 import React, {Suspense} from "react";
 
 export const meta: MetaFunction = () => {
@@ -13,26 +10,42 @@ export const meta: MetaFunction = () => {
     ];
 };
 
-
-export type LoaderData = {
-    data: Promise<{ type: string, data: ChartData[] }>;
+type Movie = {
+    id: number
+    name: string
 }
 
-export const loader: LoaderFunction = async ({request}) => {
-    const url = new URL(request.url)
-    const when = url.searchParams.get("when")
-    const thisWeek = when === "thisWeek"
-    const response = getMockData<ChartData[]>({
-        data: thisWeek ? thisWeekData : lastWeekData,
-        delay: thisWeek ? 2000 : 3000
+function getMovies(): Promise<Movie[]> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const movies = [
+                { id: 1, name: 'The Shawshank Redemption' },
+                { id: 2, name: 'The Godfather' },
+                { id: 3, name: 'The Dark Knight' },
+                { id: 4, name: 'Pulp Fiction' },
+                { id: 5, name: 'Forrest Gump' },
+                { id: 6, name: 'Inception' },
+                { id: 7, name: 'Fight Club' },
+                { id: 8, name: 'The Matrix' },
+                { id: 9, name: 'Interstellar' },
+                {
+                    id: 10,
+                    name: 'The Lord of the Rings: The Fellowship of the Ring',
+                },
+            ]
+            resolve(movies)
+        }, 2000)
     })
-    return defer<LoaderData>({data: response});
 }
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+    const movies = getMovies()
+    return defer({ movies })
+}
+
 
 export default function Index() {
-    const {data} = useLoaderData() as LoaderData
-    const {state} = useNavigation()
-
+    const {movies} = useLoaderData<typeof loader>()
     return (
         <AppShell
             header={{ height: 60 }}
@@ -43,9 +56,22 @@ export default function Index() {
             </AppShell.Header>
             <AppShell.Main>
                 <Container mt="xl">
-                    <Suspense fallback={<Charts loading/>}>
-                        <Await resolve={data}>
-                            {({data}) => <Charts data={data} loading={state === "loading"}/>
+                    <Suspense fallback={<div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                        <div>--------------------</div>
+                    </div>}>
+                        <Await resolve={movies}>
+                            {(moviesData) => <>
+                            {moviesData.map((movie)=> <div key={movie.id}> {movie.name}</div>)}
+                            </>
                             }
                         </Await>
                     </Suspense>
