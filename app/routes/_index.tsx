6ee99bd482f255e56f7371af64929dfc7fedfe61
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-09-25 17:55:43 +0300

    add defer example to show `Suspense` error with `AppShell`

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,57 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Welcome } from "~/components/Welcome/Welcome";
-import { ColorSchemeToggle } from "~/components/ColorSchemeToggle/ColorSchemeToggle";
+import {defer, LoaderFunction, MetaFunction} from "@remix-run/node";
+import {AppShell, Container} from "@mantine/core";
+import {getMockData} from "~/utils/mock.server";
+import {Await, useLoaderData, useNavigation} from "@remix-run/react";
+import {ChartData, lastWeekData, thisWeekData} from "~/utils/mockData";
+import {Charts} from "~/components/Charts/Charts";
+import React, {Suspense} from "react";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Mantine Remix App" },
-    { name: "description", content: "Welcome to Mantine!" },
-  ];
+    return [
+        {title: "Mantine Remix App"},
+        {name: "description", content: "Welcome to Mantine!"},
+    ];
 };
 
+
+export type LoaderData = {
+    data: Promise<{ type: string, data: ChartData[] }>;
+}
+
+export const loader: LoaderFunction = async ({request}) => {
+    const url = new URL(request.url)
+    const when = url.searchParams.get("when")
+    const thisWeek = when === "thisWeek"
+    const response = getMockData<ChartData[]>({
+        data: thisWeek ? thisWeekData : lastWeekData,
+        delay: thisWeek ? 2000 : 3000
+    })
+    return defer<LoaderData>({data: response});
+}
+
 export default function Index() {
-  return (
-    <div>
-      <Welcome />
-      <ColorSchemeToggle />
-    </div>
-  );
+    const {data} = useLoaderData() as LoaderData
+    const {state} = useNavigation()
+
+    return (
+        <AppShell
+            header={{ height: 60 }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <div>Logo</div>
+            </AppShell.Header>
+            <AppShell.Main>
+                <Container mt="xl">
+                    <Suspense fallback={<Charts loading/>}>
+                        <Await resolve={data}>
+                            {({data}) => <Charts data={data} loading={state === "loading"}/>
+                            }
+                        </Await>
+                    </Suspense>
+                </Container>
+            </AppShell.Main>
+        </AppShell>
+
+    );
 }
